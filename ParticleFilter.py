fixed mse calculation added beacon concentration

ParticleFilter.py
@@ -10,7 +10,7 @@ from DataTypes import WorldLimits, Point, Particle, Beacon, Agent, OdometerReadi
 
 
 class ParticleFilter:
-    def __init__(self, world_limits: WorldLimits, beacons_data: list[Beacon], waypoints_data: list[Point], freq: int, variances: Variances, speed: float, waypoint_tolerance: float, beacon_radius: float, num_particles: int):
+    def __init__(self, world_limits: WorldLimits, beacons_data: list[Beacon], waypoints_data: list[Point], freq: int, variances: Variances, speed: float, waypoint_tolerance: float, beacon_radius: float, num_particles: int, description: str, beacon_concentration: bool=False):
         self.fig = plt.figure()
         self.world_limits = world_limits
         self.beacons = beacons_data
@@ -19,6 +19,8 @@ class ParticleFilter:
         self.waypoint_tolerance = waypoint_tolerance
         self.beacon_radius = beacon_radius
         self.num_particles = num_particles
+        self.beacon_concentration = beacon_concentration
+        self.description = description
 
         self.waypoints_data = waypoints_data
 
@@ -72,6 +74,9 @@ class ParticleFilter:
 
             # predict particles by sampling from motion model with odometry info
             self.move_particles(particles=particles, odometer_reading=odometer_reading)
+            if self.beacon_concentration:
+                for beacon_data in sensors_reading.values():
+                    self.move_particles_to_circle(particles=particles, center=beacon_data[0], radius=self.beacon_radius)
 
             # calculate importance weights according to sensors readings
             weights = self.eval_weights(sensor_data=sensors_reading,
@@ -210,7 +215,11 @@ class ParticleFilter:
             weights.append(likelihood * old_weights[i])
 
         # normalize weights
-        weights = np.array(weights) / sum(weights)
+        normalizer = sum(weights)
+        if normalizer > 0:
+            weights = np.array(weights) / normalizer
+        else:
+            weights = np.ones(len(particles)) / len(particles)
 
         return weights
 
@@ -241,3 +250,15 @@ class ParticleFilter:
             new_particles.append(copy.deepcopy(particles[i]))
 
         return new_particles
+
+    @staticmethod
+    def move_particles_to_circle(particles: list[Particle], center: Point, radius: float):
+        for particle in particles:
+            dx = particle.x - center.x
+            dy = particle.y - center.y
+            distance = np.linalg.norm([dx, dy])
+            if distance > radius:
+                ratio = distance / radius
+                particle.x = dx / ratio + center.x
+                particle.y = dy / ratio + center.y
+

main.py
@@ -11,10 +11,11 @@ BEACON_RADIUS = 2.0
 SPEED = 1.0  # distance unit per time unit
 FREQ = 4  # sampling frequency per time unit
 SAVE_ANIMATION = False
-NUM_PARTICLES = [100, 200, 400, 800]
+NUM_PARTICLES = [10, 20, 40, 80]
 NUM_RUNS = 100
 PLOT_EVERY_RUN = False
-SAVE_RESULTS = True
+SAVE_RESULTS = False
+SEED = 0
 
 VARIANCES = Variances(odometer=0.1, proximity=0.4, motion=0.001, regulation=0.01)
 
@@ -50,20 +51,33 @@ def main():
                                 waypoints_data=WAYPOINTS_DATA,
                                 waypoint_tolerance=WAYPOINT_TOLERANCE,
                                 world_limits=WORLD_LIMITS,
-                                num_particles=n))
+                                num_particles=n,
+                                description=f'{n} particles',
+                                beacon_concentration=False))
+        particle_filters.append(ParticleFilter(beacon_radius=BEACON_RADIUS,
+                                beacons_data=BEACONS_DATA,
+                                freq=FREQ,
+                                speed=SPEED,
+                                variances=VARIANCES,
+                                waypoints_data=WAYPOINTS_DATA,
+                                waypoint_tolerance=WAYPOINT_TOLERANCE,
+                                world_limits=WORLD_LIMITS,
+                                num_particles=n,
+                                description=f'{n} particles, with beacon concentration',
+                                beacon_concentration=True))
     mse: dict[int: list[float]] = {}
-    se = np.empty((0, SIM_STEPS+1))
     for pf in particle_filters:
+        se = np.empty((0, SIM_STEPS + 1))
         for i in range(NUM_RUNS):
             print(f'performing run #{i+1} of {NUM_RUNS} for N={pf.num_particles}')
-            se = np.vstack([se, pf.run(sim_steps=SIM_STEPS, seed=i, plot=PLOT_EVERY_RUN)])
-        mse[pf.num_particles] = np.average(se, axis=0)
+            se = np.vstack([se, pf.run(sim_steps=SIM_STEPS, seed=i+SEED, plot=PLOT_EVERY_RUN)])
+        mse[pf.description] = np.average(se, axis=0)
 
     fig, ax = plt.subplots(1)
 
     for key, value in mse.items():
         time_indices = [k/FREQ for k in range(SIM_STEPS+1)]
-        ax.semilogy(time_indices, value, label=f'{str(key)} particles')
+        ax.semilogy(time_indices, value, label=key)
 
     ax.legend()
 